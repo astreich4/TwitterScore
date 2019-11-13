667d4fc504c4e72fe14ff05a380149618fba4da6
Adam <astreich@bu.edu>
2019-11-12 23:38:47 -0500

    A demo
    Its a lot, a whole virtual env and the app

---
+++ project_tests/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, make_response
+import requests
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST','GET'])
+def index():
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('index.html')
+
+
+@app.route('/score', methods=['POST','GET'])
+def score():
+    id = request.form['Username']
+    timeline = gettime(id)
+    number = getscore(timeline)
+    ans = getfact(number)
+    return render_template('score.html', obj=ans)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+def gettime(id):
+    #twitter api call
+    return id
+
+
+def getscore(timeline):
+    #custom score algo
+    ans = timeline
+    return ans
+
+
+def getfact(number):
+    #number api call to get fact
+    return requests.get('http://numbersapi.com/'+str(number)).text
