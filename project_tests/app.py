96291bf6ee50dc28395557a01089c14b9dc15d52
Adam <astreich@bu.edu>
2019-11-20 16:50:46 -0500

    Working Oauth
    So i managed to find time this week.... You also need to boot up a local mongodb for it to work, along with the config file

--- project_tests/app.py
+++ project_tests/app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request, make_response, session, redirect, g
-import requests
+import datetime
+import bcrypt
+import pymongo
+import json
+from flask import Flask, render_template, request, session, redirect, make_response, abort
 import tweepy
 from requests import Session
 import configs
 import funcs
-
 
 consumer_key = configs.consumer_key1
 consumer_secret = configs.consumer_secret1
@@ -12,66 +14,78 @@ consumer_secret = configs.consumer_secret1
 callback = 'http://127.0.0.1:5000/oauthhome'
 
 app = Flask(__name__)
-
-global sess
 sess = Session()
-
 app.config.from_object("configs.Config")
+app.config['SESSION_TYPE'] = 'filesystem'
+client = pymongo.MongoClient("localhost",27017)
+db = client.test
+users = db.users
+sysinfo = db.sysinfo
 
 
 
-@app.route('/', methods=['POST','GET'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        pass
-    else:
-        return render_template('index.html')
+    #post = {"author": "Mike","text": "My first blog post!","tags": ["mongodb", "python", "pymongo"],}
+    #users.insert({"name":"helloworlds"})
+    return render_template("front.html")
 
-
-@app.route('/auth', methods=['POST','GET'])
+@app.route('/auth')
 def auth():
-
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret, callback)
     url = auth.get_authorization_url()
-    session['request_token'] = auth.request_token['oauth_token']
-    print("auth",session)
-    print(url)
-    url = redirect(url)
-    print(url)
-    return url
+    session['request_token'] = auth.request_token
+    return redirect(url)
 
 
-@app.route('/oauthhome', methods=['POST','GET'])
+@app.route('/oauthhome')
 def twitter_callback():
-    """
-print("callback",session)
-    token = session.get('request_token')
-    print(token)
-"""
-
-
-
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret, callback)
 
     verifier = request.args.get('oauth_verifier')
     token = request.args.get('oauth_token')
     auth.request_token = {'oauth_token': token,
                           'oauth_token_secret': verifier}
-    print(auth.request_token)
-    sessio = (auth.access_token, auth.access_token_secret)
-    print("callback2",sessio)
+    print("test1", auth.request_token)
+    #must run auth.get_access_token
+    print("test2", auth.get_access_token(verifier))
+    at = auth.access_token
+    ats = auth.access_token_secret
+    sessio = (at, ats)
+    print("callback2", sessio)
 
-    return redirect('/app')
-
-@app.route('/app', methods=['POST','GET'])
-def request_twitter():
-    print(session)
-    token, token_secret = session['token']
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret, callback)
-    auth.set_access_token(token, token_secret)
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(at, ats)
     api = tweepy.API(auth)
+    user = api.me()
+    screenname = user.screen_name
 
-    return api.me()
+    salt =bcrypt.gensalt()
+    hashed = bcrypt.hashpw(ats.encode('utf8'),salt)
+    hashed = hashed.decode()
+    users.insert({"screen_name": screenname, "access_token": at, "access_token_secret": ats, "atshash": hashed, "salt": salt})
+    resp = make_response(redirect('/home'))
+    resp.set_cookie('id', value=hashed, httponly=True)
+    return resp
+
+@app.route('/home')
+def home():
+    if 'id' in request.cookies:
+        #check if its the same
+        test = request.cookies.get('id')
+        comp = db.users.find_one({"atshash": test})
+        comp = comp["atshash"]
+        print(test)
+        print(comp)
+        if test == comp:
+            resp = make_response("user logged on")
+        else:
+            #print("error 1")
+            abort(401)
+    else:
+        print("error2")
+        abort(401)
+    return resp
 
 @app.route('/score', methods=['POST','GET'])
 def score():
