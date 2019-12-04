ecd87bca3728edc36c2046480b8e2162b61d3dd2
Adam <astreich@bu.edu>
2019-12-04 15:11:14 -0500

    Score
    Counts favorites of recent tweets

--- project_tests/funcs.py
+++ project_tests/funcs.py
@@ -12,14 +12,21 @@ def gettime(at ,ats):
     auth.set_access_token(at, ats)
     api = tweepy.API(auth)
     timeline = api.user_timeline()
-    print(timeline)
     return timeline
 
 
 def getscore(timeline):
-    #custom score algo
-    ans = timeline
-    return 4
+    #custom score algo, currently just a sum of your most recent favorites
+    print(len(timeline))
+    numtweets = len(timeline)
+    temp = timeline[0]._json
+    count = 0
+    favcount = 0
+    while count < numtweets:
+        temptweet = timeline[count]._json
+        favcount += temptweet["favorite_count"]
+        count += 1
+    return favcount
 
 
 def getfact(number):
